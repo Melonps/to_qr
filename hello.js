0b1143d17c11d13fb800c4664acd5f804cb23ff0
Melops <95263124+Melonps@users.noreply.github.com>
2022-04-24 16:30:46 +0900

    [FIX] ミスの修正 #5
    くそ時間かかったわ！

--- hello.js
+++ hello.js
@@ -1,14 +1,13 @@
 document.getElementById("change_color").onclick = () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.scripting.executeScript({
-            target: { tableId: tabs[0].Id },
+            target: { tabId: tabs[0].id },
             function: changeBackgroundColor,
         });
     });
 };
 
 function changeBackgroundColor() {
-    var body = document.getElementById("popup_body");
-    body.style.backgroundColor ="#219ddd";
+    document.body.style.backgroundColor ="#219ddd";
     console.log("背景変更");
-};
+}
