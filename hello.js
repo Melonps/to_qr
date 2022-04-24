72a666ccd80403f206c404f424ef4065d44dcf6a
Melops <95263124+Melonps@users.noreply.github.com>
2022-04-24 17:21:27 +0900

    [ADD] 履歴を表示機能の追加 #6
    ミスが怖い

--- hello.js
+++ hello.js
@@ -1,9 +1,17 @@
-document.getElementById("change_color").onclick = () => {
-    changeBackgroundColor();
-}
-
-function changeBackgroundColor() {
-    var body = document.getElementById("popup_body");
-    body.style.backgroundColor ="#219ddd";
-    console.log("背景変更");
+document.getElementById("view_history").onclick = () => {
+    chrome.runtime.sendMessage({ to_backscript: "tap_button" });
 };
+
+chrome.runtime.onMessage.addListener(function (Message, sender, sendResponse) {
+    if (message.history_list) {
+        let history_list = document.getElementById("history_list");
+        history_list.innerHTML = "";
+        var list = message.history_list;
+        console.log(list);
+        for (var i = list.length; i > 0; i--) {
+            var history_url = document.createElement("p");
+            history_url.textContent = list[i];
+            history_list.appendChild(history_url);
+        }
+    }
+});
