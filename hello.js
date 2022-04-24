eb5140473a14976af4f431c3fad336ad8c34c689
Melops <95263124+Melonps@users.noreply.github.com>
2022-04-24 15:43:33 +0900

    [FIX] ミス修正2 #5
    しますた

--- hello.js
+++ hello.js
@@ -1,6 +1,11 @@
 document.getElementById("change_color").onclick = () => {
-    changeBackgroundColor();
-}
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.scripting.executeScript({
+            target: { tableId: tabs[0].Id },
+            function: changeBackgroundColor,
+        });
+    });
+};
 
 function changeBackgroundColor() {
     var body = document.getElementById("popup_body");
